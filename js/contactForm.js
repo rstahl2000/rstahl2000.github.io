15a9dfeac92a728f895b965375e18261bb5123af
rstahl2000 <rstahl@udel.edu>
2021-10-02 11:17:32 -0400

    Creates JS file for validation
    Adds code to the javascript form that validates all the fields in the html form. Also adds minimal code into the contact html to implement the script.

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,4 +1,28 @@
-document.getElementById('contact').addEventListener('submit',checkInput)
-function checkInput(){
-    
+function validateForm(){
+    let isValid=true
+    let x = document.forms["contact"]["user_fname"].value;
+    if (x == "") {
+        alert("First name must be filled out");
+        isValid=false
+    }
+    x=document.forms["contact"]["user_lname"].value;
+    if (x == "") {
+        alert("Last name must be filled out");
+        isValid=false
+    }
+    x=document.forms["contact"]["user_email"].value;
+    if (x.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g)==null) {
+        alert("Email must be proper format: XYZ@domain.headdomain");
+        isValid=false
+    }
+    x=document.forms["contact"]["user_phone"].value;
+    if (x.match(/^([0-9]{3})-([0-9]{3})-([0-9]{4})$/g)==null) {
+        alert("Phone must be proper format: 123-456-7890");
+        isValid=false
+    }
+    x=document.forms["contact"]["user_message"].value;
+    if (x == "") {
+        alert("A message must be left.");
+        isValid=false
+    }
 }
